bf34832a314fd527b9ed7d531f221d05625a36c2
Anton Dziavitsyn <a.dziavitsyn@invento.by>
2017-07-25 13:42:16 +0300

    added best_scan + get_user_scans

--- api/web_actions/best_scan.py
+++ api/web_actions/best_scan.py
@@ -1,21 +1,49 @@
-import sys
 import logging
 from flask import Blueprint, jsonify, request, abort
 
-sys.path.append('/')
-from data.repositories import UserRepository, ScanRepository
-from data.models import Scan
+from data.repositories import UserRepository, ScanRepository, ScannerModelRepository, ScanMetricRepository, ScanMetricValueRepository
+from data import data_connection
+from calculations.fitting_algorithms.get_best_scan import get_best_scan
 
 logger = logging.getLogger(__name__)
 
 _userRep = UserRepository()
 _scanRep = ScanRepository()
+_scanModelRep = ScannerModelRepository()
+_scanMetricRep = ScanMetricRepository()
+_scanMetricValueRep = ScanMetricValueRepository()
 
 
-def get_best_foot_scan(user):
+def extract_calculation_attributes(scan, metrics):
+    attrs = {}
+    for metr in metrics:
+        val = _scanMetricValueRep.get(dict(scan=scan, metric=metr))
+        if len(val) > 0:
+            val = val[0]
+            try:
+                attrs[metr.name] = float(val.value)
+            except ValueError:
+                False
+
+    return attrs
+
+
+def get_best_foot_scan(user, scanner_model):
     scans_data = {}
+    _graph = data_connection.get_graph()
+    scans = _scanRep.get_scans(user, scanner_model)
+    metrics = _scanMetricRep.get(dict(scanner_model=scanner_model))
 
-    return 1,1,1,1
+    for scan in scans:
+        if not scan.scan_id in scans_data:
+            scans_data[scan.scan_id] = {}
+
+        if _graph.element_from_link(scan.model_type).name == 'LEFT_FOOT':
+            scans_data[scan.scan_id]['LEFT'] = extract_calculation_attributes(scan, metrics)
+        elif _graph.element_from_link(scan.model_type).name == 'RIGHT_FOOT':
+            scans_data[scan.scan_id]['RIGHT'] = extract_calculation_attributes(scan, metrics)
+
+    return get_best_scan(scans_data)
 
 
 best_scan_action = Blueprint('best_scan_action', __name__)
@@ -30,8 +58,17 @@ def best_scan():
     else:
         user = user[0]
 
-    sc = _scanRep.get(dict(user=user))
+    model = request.args.get('scanner_model')
+    scanner_model = _scanModelRep.get(dict(name=model))
+    if len(scanner_model) == 0:
+        abort(404, 'scanner_model not found')
+    else:
+        scanner_model = scanner_model[0]
 
-    get_best_foot_scan(user)
+    user_best_scan, user_best_scan_dist, scans_count, metrics_count = get_best_foot_scan(user, scanner_model)
 
-    return 'test'
+    return jsonify({'scan_id': user_best_scan,
+                    'distance': user_best_scan_dist,
+                    'scans_processed': scans_count,
+                    'metrics_count': metrics_count
+                    })
--- data/repositories/ScanRepository.py
+++ data/repositories/ScanRepository.py
@@ -6,3 +6,14 @@ class ScanRepository(RepositoryBase):
 
     def __init__(self):
         super().__init__(Scan)
+
+    def get_scans(self, user, scanner_model):
+        from .ScannerRepository import ScannerRepository
+        from data import data_connection
+
+        graph = data_connection.get_graph()
+        _scannerRep = ScannerRepository()
+        scanners = [scanner._id for scanner in _scannerRep.get(dict(model=scanner_model))]
+
+        scans = self.get(dict(user=user))
+        return [scan for scan in scans if graph.element_from_link(scan.scanner)._id in scanners]
