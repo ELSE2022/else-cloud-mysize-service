aa7d48d7a7ab755201eafe2a9d034b9a4de5d6b9
Anton Dziavitsyn <a.dziavitsyn@invento.by>
2017-07-25 13:00:08 +0300

    Revert "get_user_scans done"
    This reverts commit 5bd79530408e20f1c98d53fdd55332a833cd20cf.

--- api/init.py
+++ api/init.py
@@ -1,9 +1,8 @@
 from .data_controller import data_controller
 from .web_actions.best_scan import best_scan_action
-from .web_actions.get_user_scans import get_user_scans_action
 
 
 def register_controllers(app):
     app.register_blueprint(data_controller)
     app.register_blueprint(best_scan_action)
-    app.register_blueprint(get_user_scans_action)
+
