69ebc70510f261ab3e1806d0c1f80e9be0e42d07
Anton Dziavitsyn <a.dziavitsyn@invento.by>
2017-07-31 18:34:38 +0300

    added + best_style (metric by size) server action

--- api/init.py
+++ api/init.py
@@ -2,7 +2,7 @@ from .data_controller import data_controller
 from .web_actions.best_scan import best_scan_action
 from .web_actions.get_user_scans import get_user_scans_action
 from .web_actions.generate_comparison_config import generate_comparison_config_action
-from .web_actions.best_size import best_size_action
+from .web_actions.best_size import best_size_action, best_style_action
 
 
 def register_controllers(app):
@@ -11,3 +11,4 @@ def register_controllers(app):
     app.register_blueprint(get_user_scans_action)
     app.register_blueprint(generate_comparison_config_action)
     app.register_blueprint(best_size_action)
+    app.register_blueprint(best_style_action)
--- api/web_actions/best_size.py
+++ api/web_actions/best_size.py
@@ -1,15 +1,15 @@
 import logging
 from flask import Blueprint, jsonify, request, abort
 from ..authentication import requires_auth
-from data.repositories import ProductRepository, UserRepository, ScanRepository, ModelRepository, ComparisonRuleMetricRepository, ScanMetricValueRepository, ModelMetricValueRepository
+from data.repositories import ProductRepository, UserRepository, UserSizeRepository, ScanRepository, ComparisonRuleMetricRepository, ScanMetricValueRepository, ModelMetricValueRepository
 from data import data_connection
 from calculations.fitting_algorithms.get_metrics_by_sizes import get_metrics_by_sizes
 
 logger = logging.getLogger(__name__)
 _productRep = ProductRepository()
 _userRep = UserRepository()
+_userSizeRep = UserSizeRepository()
 _scanRep = ScanRepository()
-_modelRep = ModelRepository()
 _compRuleMetricRep = ComparisonRuleMetricRepository()
 _scanMetrValueRep = ScanMetricValueRepository()
 _modelMetrValueRep = ModelMetricValueRepository()
@@ -54,7 +54,7 @@ def get_foot_best_size(product, model_types, scans):
     return all_results
 
 
-def generate_result(data):
+def generate_result(data, user_size=None):
     sizes = []
     avg_result = []
     count_typ = 0
@@ -81,11 +81,16 @@ def generate_result(data):
             'size_type': 'FOOT'
             }
 
-    return {
-        'prev_best_size': result(max_idx - 1),
-        'best_size': result(max_idx),
-        'next_best_size': result(max_idx + 1),
-    }
+    if user_size is None:
+        return {
+            'prev_best_size': result(max_idx - 1),
+            'best_size': result(max_idx),
+            'next_best_size': result(max_idx + 1),
+        }
+    else:
+        return {
+            'best_style': result(sizes.index(user_size.string_value))
+        }
 
 
 
@@ -121,3 +126,46 @@ def best_size():
     result = get_foot_best_size(product, model_types, scans)
 
     return jsonify(generate_result(result))
+
+best_style_action = Blueprint('best_style_action', __name__)
+
+
+@best_style_action.route('/fitting/best_style')
+@requires_auth
+def get_best_size_metric_by_size():
+    _graph = data_connection.get_graph()
+
+    product_uuid = request.args.get('product')
+
+    product = _productRep.get(dict(uuid=product_uuid))
+    if len(product) == 0:
+        abort(404, 'product not found')
+    else:
+        product = product[0]
+
+    user_uuid = request.args.get('user')
+    user = _userRep.get(dict(uuid=user_uuid))
+    if len(user) == 0:
+        abort(404, 'user not found')
+    else:
+        user = user[0]
+
+    model_types = _graph.elements_from_links(_graph.element_from_link(product.default_comparison_rule).model_types)
+
+    user_size = _userSizeRep.get(dict(user=user))
+
+    size = None
+    for s in user_size:
+        _size = _graph.element_from_link(s.size)
+        if _graph.elements_from_links(_size.model_types) == model_types:
+            size = _size
+    if size is None:
+        abort(404, 'user\'s size not found')
+
+    scans = []
+    for ty in model_types:
+        scans += _scanRep.get(dict(user=user, is_default=True, model_type=ty))
+
+    result = get_foot_best_size(product, model_types, scans)
+
+    return jsonify(generate_result(result, size))
