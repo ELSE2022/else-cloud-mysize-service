b98507645a1b282b411cf584e9c09566ce1466dc
Maxim Cherchuk <maxim.cherchuk@gmail.com>
2017-08-04 14:21:43 +0300

    create_user function made safe

--- api/web_actions/create_user.py
+++ api/web_actions/create_user.py
@@ -17,54 +17,72 @@ _modelTypeRep = ModelTypeRepository()
 
 create_user_action = Blueprint('create_user_action', __name__)
 
+
 @create_user_action.route('/fitting/create_user')
 def create_user():
     user_uuid = request.args.get('user')
     size_value = request.args.get('size', '35')
     model = request.args.get('type')
 
-    model_type = _modelTypeRep.get({
-        'name': model,
-    })
+    user = check_user(user_uuid)
+    model_type = get_model_type(model)
+    size = get_size(size_value, model_type)
+    check_user_sizes(user, size)
 
-    if len(model_type) < 1:
-        left_foot = _modelTypeRep.get({
-            'name': 'LEFT_FOOT',
-        })[0]
-        right_foot = _modelTypeRep.get({
-            'name': 'RIGHT_FOOT',
-        })[0]
-        model_type = [left_foot, right_foot]
+    return jsonify({ 'default_size': 'FOOT - ' + size_value })
 
+
+def get_model_type(model):
+    if model is not None:
+        return _modelTypeRep.get({ 'name': model })
+    else:
+        left_foot = _modelTypeRep.get({ 'name': 'LEFT_FOOT' })
+        right_foot = _modelTypeRep.get({ 'name': 'RIGHT_FOOT' })
+        if len(left_foot) == 0:
+            abort(404, 'ModelType \'LEFT_FOOT\' not exist')
+        if len(right_foot) == 0:
+            abort(404, 'ModelType \'RIGHT_FOOT\' not exist')
+        return [left_foot[0], right_foot[0]]
+
+
+def get_size(size_value, model_type):
     size = _sizeRep.get({
         'string_value': size_value,
         'model_types': model_type,
     })
 
-    if len(size) < 1:
-        size = _sizeRep.add({
+    if len(size) != 0:
+        return size[0]
+    else:
+        return _sizeRep.add({
             'string_value': size_value,
             'model_types': model_type,
         })
-    else:
-        size = size[0]
 
-    user = _userRep.add({
-        'uuid': user_uuid,
-    })
 
+def check_user_sizes(user, size):
     user_sizes = _userSizeRep.get({
         'user': user,
         'size': size,
     })
-
-    if len(user_sizes) < 1:
+    if len(user_sizes) != 0:
+        return False
+    else:
         user_sizes = _userSizeRep.add({
             'user': user,
             'size': size,
             'creation_time': str(datetime.now()),
         })
+        return True
 
-    return jsonify({
-        'default_size': 'FOOT - ' + size_value,
-    })
+
+def check_user(user_uuid):
+    if user_uuid is None:
+        abort(400, 'Request malformed: \'user\' argument not passed')
+
+    user = _userRep.get({ 'uuid': user_uuid })
+
+    if len(user) == 0:
+        return _userRep.add({ 'uuid': user_uuid })
+    else:
+        abort(400, 'User with uuid=\'{}\' already exists'.format(user_uuid))
