bf6d728124f9e9ae3e140b96870a2bbfa84adefd
Maxim Cherchuk <maxim.cherchuk@gmail.com>
2017-08-24 14:39:14 +0300

    returns are added to container's functions

--- data/Container.py
+++ data/Container.py
@@ -6,20 +6,20 @@ class Container(object):
     name: str = None
 
     def __init__(self, conn, name):
-        self.connection = conn
+        self.__connection = conn
         self.name = name
 
     def add_object(self, name, content):
-        self.__connection.put_object(self.name, name, contents=content, content_type='text/plain')
+        return self.__connection.put_object(self.name, name, contents=content, content_type='text/plain')
 
     def list_objects(self):
-        self.__connection.get_container(self.name)
+        return self.__connection.get_container(self.name)
 
     def get_object(self, name):
-        self.__connection.get_object(self.name, name)
+        return self.__connection.get_object(self.name, name)
 
     def delete_object(self, name):
-        self.__connection.delete_object(self.name, name)
+        return self.__connection.delete_object(self.name, name)
 
     def delete(self):
-        self.__connection.delete_container(self.name)
+        return self.__connection.delete_container(self.name)
